fed624eff7fad332e5f8229c8c7d6639fe3314aa
Niamat <naimatullah8848@gmail.comgit confiq --global user.name naimatdevgit commit -m first>
2023-06-08 22:01:17 +0500

    first commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,5 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Document</title>
+    <h1>welcome</h1>
 </head>
 <body>
